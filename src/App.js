938eecb135a87dab6e796f0744fa1e6343cf8471
Hadi Khan <khan.hadi2951@gmail.com>
2023-05-27 03:21:46 +0500

    Button added to update IPset

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ import {
   Accordion,
   AccordionDetails,
   TextField,
+  Button, // Add the Button component import
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import './App.css';
+import axios from 'axios';
 
 const StyledExpandMoreIcon = withStyles({
   root: {
@@ -40,18 +42,9 @@ export default function App() {
   const classes = useStyles();
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredData, setFilteredData] = useState({
-    blocked: statusData.Blocked.map(ip => ({ ip, status: 'blocked' })),
-    whitelist: statusData.WhiteList.map(ip => ({ ip, status: 'whitelisted' })),
+    blocked: statusData.Blocked.map((ip) => ({ ip, status: 'blocked' })),
+    whitelist: statusData.WhiteList.map((ip) => ({ ip, status: 'whitelisted' })),
   });
-  useEffect(() => {
-    const filteredBlocked = statusData.Blocked.filter((row) =>
-      row.toLowerCase().includes(searchTerm.toLowerCase())
-    ).map(ip => ({ ip, status: 'blocked' }));
-    const filteredWhitelist = statusData.WhiteList.filter((row) =>
-      row.toLowerCase().includes(searchTerm.toLowerCase())
-    ).map(ip => ({ ip, status: 'whitelisted' }));
-    setFilteredData({ blocked: filteredBlocked, whitelist: filteredWhitelist });
-  }, [searchTerm]);
   const AccordionSummary = withStyles({
     root: {
       backgroundColor: 'black',
@@ -63,91 +56,137 @@ export default function App() {
     },
     expanded: {},
   })(MuiAccordionSummary);
+
+  useEffect(() => {
+    const filteredBlocked = statusData.Blocked.filter((row) =>
+      row.toLowerCase().includes(searchTerm.toLowerCase())
+    ).map((ip) => ({ ip, status: 'blocked' }));
+    const filteredWhitelist = statusData.WhiteList.filter((row) =>
+      row.toLowerCase().includes(searchTerm.toLowerCase())
+    ).map((ip) => ({ ip, status: 'whitelisted' }));
+    setFilteredData({ blocked: filteredBlocked, whitelist: filteredWhitelist });
+  }, [searchTerm]);
+
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
+
+  const dispatchWorkflow = async () => {
+    const owner = 'okHadi';
+    const repo = 'get-IPsets-WAF';
+    const workflowId = '56979211';
+    const branchName = 'main';
+    const accessToken = process.env.REACT_APP_ACCESS_TOKEN; // Replace with your access token
+
+    const url = `https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflowId}/dispatches`;
+
+    try {
+      const response = await axios.post(
+        url,
+        {
+          ref: branchName,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      console.log('API Response:', response.data);
+    } catch (error) {
+      console.error('Error dispatching workflow:', error);
+    }
+  };
+
   return (
     <div>
       <h1>BP WAF PRODUCTION</h1>
       <div className="accordion-container">
-  <AccordionDetails className={classes.accordionDetails}>
-    <Accordion defaultExpanded>
-      <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
-        <h3>Blocked IPsets</h3>
-      </AccordionSummary>
-      <AccordionDetails className={classes.accordionDetails}>
-      <div className ="search-container">
-        <TextField
-          label="Search"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          fullWidth
-          margin="normal"
-          variant="outlined"
-        />
-          </div>
+        <AccordionDetails className={classes.accordionDetails}>
+          <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
+              <h3>Blocked IPsets</h3>
+            </AccordionSummary>
+            <AccordionDetails className={classes.accordionDetails}>
+              <div className="search-container">
+                <TextField
+                  label="Search"
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
 
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="Blocked IPset Table">
-            <TableHead>
-              <TableRow>
-                <TableCell>IP Address</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredData.blocked.map((ip) => (
-                <TableRow key={ip.ip}>
-                  <TableCell component="th" scope="row">
-                    {ip.ip}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </AccordionDetails>
-    </Accordion>
-  </AccordionDetails>
-  <div className="accordion-container">
-  <AccordionDetails className={classes.accordionDetails}>
-    <Accordion defaultExpanded>
-      <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
-        <h3>WhiteList IPsets</h3>
-      </AccordionSummary>
-      <AccordionDetails className={classes.accordionDetails}>
-      <div className ="search-container">
-        <TextField
-          label="Search"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          fullWidth
-          margin="normal"
-          variant="outlined"
-        />
+              <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="Blocked IPset Table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>IP Address</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {filteredData.blocked.map((ip) => (
+                      <TableRow key={ip.ip}>
+                        <TableCell component="th" scope="row">
+                          {ip.ip}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </AccordionDetails>
+          </Accordion>
+        </AccordionDetails>
+        <div className="accordion-container">
+          <AccordionDetails className={classes.accordionDetails}>
+            <Accordion defaultExpanded>
+              <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
+                <h3>WhiteList IPsets</h3>
+              </AccordionSummary>
+              <AccordionDetails className={classes.accordionDetails}>
+                <div className="search-container">
+                  <TextField
+                    label="Search"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </div>
+                <TableContainer component={Paper}>
+                  <Table className={classes.table} aria-label="Blocked IPset Table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>IP Address</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {filteredData.whitelist.map((ip) => (
+                        <TableRow key={ip.ip}>
+                          <TableCell component="th" scope="row">
+                            {ip.ip}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </AccordionDetails>
+            </Accordion>
+          </AccordionDetails>
         </div>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="Blocked IPset Table">
-            <TableHead>
-              <TableRow>
-                <TableCell>IP Address</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredData.whitelist.map((ip) => (
-                <TableRow key={ip.ip}>
-                  <TableCell component="th" scope="row">
-                    {ip.ip}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </AccordionDetails>
-    </Accordion>
-  </AccordionDetails>  
-</div> 
-</div>   
+      </div>
+
+      {/* Button to trigger API request */}
+      <Button variant="contained" color="primary" onClick={dispatchWorkflow}>
+        Dispatch Workflow
+      </Button>
     </div>
   );
 }
