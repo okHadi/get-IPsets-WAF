2f8c6a7d8257bec3904f6e611c6ce11bca77906c
Hadi Khan <khan.hadi2951@gmail.com>
2023-07-06 17:02:38 +0500

    edited workflow, final testing

--- s3_ip_set.py
+++ s3_ip_set.py
@@ -4,8 +4,11 @@ import os
 with open ('ip-descriptions.json') as ip_descriptions_s3:
     ip_descriptions_s3 = json.load(ip_descriptions_s3)
 
-new_ip_with_description = os.environ.get("NEW_IP_WITH_DESCRIPTION")
-ip_descriptions_s3["ip_and_description"].append(new_ip_with_description)
+with open('new-ip.json') as new_ip:
+    new_ip = json.load(new_ip)
+
+
+ip_descriptions_s3["ip_and_description"].append(new_ip)
 
 with open('ip-descriptions.json', 'w') as outfile:
     json.dump(ip_descriptions_s3, outfile)
